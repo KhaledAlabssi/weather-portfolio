89cc76e0099254b85725608663a00a0022f4467b
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-27 19:55:31 +0200

    adding my api variables, setting the app ready to start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
 
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 function App() {
   return (
     <div className="App">
+      hi
       
     </div>
   );
